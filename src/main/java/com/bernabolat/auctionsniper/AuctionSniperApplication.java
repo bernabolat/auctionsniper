f488a7fa6aa087cabf45b0ff1e5273d741a43b1d
Berna Bolat <bernadeneri@gmail.com>
2024-04-18 21:33:15 -0400

    initial commit

---
+++ src/main/java/com/bernabolat/auctionsniper/AuctionSniperApplication.java
@@ -0,0 +1,13 @@
+package com.bernabolat.auctionsniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSniperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionSniperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bernabolat/auctionsniper/AuctionSniperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernabolat.auctionsniper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSniperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
